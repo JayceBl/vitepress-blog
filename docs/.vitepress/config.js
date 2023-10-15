5290288998dcd480d62fe5e34bd81d1719c2e174
jayce <1102408100@qq.com>
2023-10-15 10:24:23 +0800

    config: update configjs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vitepress";
 
 export default defineConfig({
-  base: 'vitepress-blog',
+  base: '/vitepress-blog/',
   title: "My Awesome Blog",
   description: "A VitePress Site",
   themeConfig: {
